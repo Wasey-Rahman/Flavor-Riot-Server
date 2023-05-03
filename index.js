99528da01412778ff9640014b2673357f2cb9312
Wasey Rahman <wasey123435@gmail.com>
2023-05-03 20:24:25 +0600

    chef data

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+
+const chef =require ('./data/chef.json');
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
+  });
+
+
+app.get('/chef', (req, res) => {
+    res.send(chef);
+  });
+
+
+app.get('/chef/:id', (req, res) => {
+    const id =req.params.id;
+    console.log(id);
+    const selectedChef=chef.find(n=> n.id ==id);
+    res.send(selectedChef)
   });
 
   
